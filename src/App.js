ecf21ae52c66c9595dc28c2f04c2987f5b69fa6a
ROHAN-W11 <rohanrathi333@gmail.com>
2023-09-11 13:01:29 +0530

    Add - adding code for Speech to Text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from "react";
+
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+
+const App = () => {
+  const { 
+    transcript, 
+    listening, 
+    resetTranscript, 
+    browserSupportsSpeechRecognition 
+  } = useSpeechRecognition();
+
+  if(!browserSupportsSpeechRecognition) {
+    return <span>Your Browser doesn't support Speech to Text</span>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{transcript}</p>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>      
+      <p>Microphone: {listening ? 'ON' : 'OFF'}</p>
     </div>
   );
-}
+};
 
 export default App;
